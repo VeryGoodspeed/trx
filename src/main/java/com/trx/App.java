e50bc603243d8b084f5f85b333a2fa1e593efc0e
e <erickomar.ricardez@gmail.com>
2023-11-05 22:25:20 -0600

    first commit

---
+++ src/main/java/com/trx/App.java
@@ -0,0 +1,13 @@
+package com.trx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
